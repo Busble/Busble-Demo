3928a4be09a971e177b4c72fa6fef45ff58f5847
YoonuchReal <Yoonuch@outlook.com>
2019-03-06 22:54:09 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,144 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p class="text">
-      Hello World!
-    </p>
+  <div class="app-home app-container">
+    <header>
+      <div class="">
+        <div class="img">
+            <img class="bg" src="./../assets/img/home/header/header-bg.svg" alt="" />
+        </div>
+        <div class="header-content">
+          <div class="title wrap">ผู้คนมากมาย รอชื่นชมผลงานของคุณอยู่.</div>
+          <div class="tagline wrap">Design better. Faster. And get beautiful results forever.</div>
+        </div>
+        <div class="call-to-action">
+          <div class="button primary">สมัครสิหรรรรรรรม</div>
+          <div class="button">สุบอลลล</div>
+        </div>
+        <div class="img2">
+            <img src="./../assets/img/home/header/header-mockups.png" alt="" />
+        </div>
+      </div>
+    </header>
+    <div class="section-02">
+      <img class="top" src="" alt="" />
+      <div class="content">
+      </div>
+      <img class="bottom" src={section02Bottom} alt="" />
+    </div>
+    <div class="section-03"></div>
+    <div class="footer"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.hello {
-  .text {
-    color: red;
+.wrap {
+    width: 100%;
+    max-width: 800px;
+    margin: auto;
   }
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  
+  header {
+    position: relative;
+    text-align: center;
+    overflow: hidden;
+    min-height: 450;
+    background-color: #FFF;
+    img.bg {
+        position: absolute;
+        top: 50px;
+        right: 0;
+        bottom: 30px;
+        width: 80%;
+        height: 80%;
+        z-index: 0;
+    }
+    .header-content {
+        position: relative;
+        width: 100%;
+        height: auto;
+        padding: 60px 0 0 0;
+        z-index: 1000;
+        .title {
+            position: relative;
+            font-size: 45px;
+            line-height: 55px;
+            font-weight: bold;
+            margin: 10px auto;
+            padding: 0 20px;
+            color: #333333;
+        }
+        .tagline {
+            position: relative;
+            font-size: 20px;
+            margin-top: 10px;
+            margin-bottom: 10px;
+            padding-left: 20px;
+            padding-right: 20px;
+            color: #7B868A;
+        }
+    }
+    .call-to-action {
+        position: relative;
+        padding: 10px;
+        .button {
+            cursor: pointer;
+            display: inline-block;
+            font-size: 18px;
+            font-weight: bold;
+            padding: 12px 24px;
+            margin: 10px;
+            border: 1px solid #E9EFF5;
+            border-radius: 3px;
+            color: #333;
+            background-color: #FFF;
+            transition: all .2s ease;
+            box-shadow: 0 5px 15px rgba(32,43,54,0.05);
+            &.primary {
+                border: 1px solid #0469B9;
+                color: #FFF;
+                background-color: #0469B9;
+            }
+            &:hover {
+                box-shadow: 0 5px 15px rgba(32,43,54,0.1);
+            }
+        }
+    }
+    .img2 {
+        position: relative;
+        width: 100%;
+        max-width: 1080px;
+        height: auto;
+        margin: auto;
+        z-index: 1000;
+        img {
+            position: relative;
+            width: 100%;
+            height: 100%;
+        }
+    }
+  }
+  .section-02 {
+    height: auto;
+    background-color: #fafafc;
+    .top {
+        margin-top: -1px;
+    }
+    .bottom {
+        margin-bottom: -6px;
+    }
+    .content {
+        height: 400px;
+    }
+  
+  }
+  .section-03 {
+    height: 1800px;
+  }
+  .footer {
+    height: 400px;
+    background-color: #272C34;
+  }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="app-navbar">
+    <div class="app-navbar">
+      <div class="container">
+        <div class="left">
+          <div class="logo">
+            <div class="img"></div>
+            <div class="text">Busble</div>
+          </div>
+        </div>
+        <div class="right">
+          <div class="nav-desktop">
+            <a href="#" class="active">Home</a>
+            <a href="#">Pricing</a>
+            <a href="#">Blog</a>
+            <a href="#">Help</a>
+          </div>
+          <div class="button"></div>
+        </div>
+      </div>
+    </div>
+    <div class="app-navbar-zone"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+        handleScroll: function () {
+            this.console.log("sdfsa");
+        }
+    },
+
+    created: function () {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    destroyed: function () {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.app-navbar-t {
+  background-color: red;
+}
+.app-navbar {
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    max-width: 1440px;
+    margin: auto;
+    background-color: #fff;
+    z-index: 5000;
+    .container {
+      margin: auto;
+      .left {
+        float: left;
+        .logo {
+          display: inline-block;
+          vertical-align: middle;
+          .img {
+            display: inline-block;
+            vertical-align: middle;
+            width: 60px;
+            height: 60px;
+            background-color: #0469B9;
+          }
+          .text {
+            display: inline-block;
+            vertical-align: middle;
+            font-weight: bold;
+            font-size: 18px;
+            margin-left: 10px;
+            color: rgb(61, 64, 71);
+          }
+        }
+      }
+      .right {
+        float: right;
+        .button {
+          position: relative;
+          display: inline-block;
+          vertical-align: middle;
+          width: 60px;
+          height: 60px;
+        }
+        .nav-desktop {
+          display: inline-block;
+          vertical-align: middle;
+          a {
+            display: inline-block;
+            vertical-align: middle;
+            font-weight: bold;
+            font-size: 16px;
+            line-height: 18px;
+            margin: 0 10px;
+            color: #7B868A;
+            &.active {
+              color: #0469B9;
+            }
+          }
+        }
+      }
+    }
+  }
+  .app-navbar-active {
+    -webkit-box-shadow: 0px -9px 36px 0px rgba(0,0,0,0.2);
+    -moz-box-shadow: 0px -9px 36px 0px rgba(0,0,0,0.2);
+    box-shadow: 0px -9px 36px 0px rgba(0,0,0,0.2);
+  }
+  .app-navbar-zone {
+    width: 100%;
+    height: 60px;
+  }
+  
+  @media only screen and (max-width: 600px) {
+    .nav-desktop {
+      display: none !important;
+    }
+  }
+</style>
