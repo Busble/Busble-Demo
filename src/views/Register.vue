5a26135572709c4498374ff1bb48d1c6389b6aec
YoonuchReal <Yoonuch@outlook.com>
2019-03-08 04:57:40 +0700

    update homepage

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,72 +1,126 @@
 <template>
   <div class="app-container">
-    <div class="resgiter-form">
-      <div class="resgiter-form--dialog">
-        <div class="header">
-          <h1>สมัครสิ่</h1>
-        </div>
-        <div class="content">
-          <form @submit.prevent="submit">
-            <input type="text" v-model="text_email">
-            <button type="submit">submit</button>
-          </form>
-        </div>
-      </div>
+    <div class="process-mockup">
+      <img class="bg" src="./../assets/img/register/prototyping_process.svg" alt>
     </div>
+    <div class="content">
+      <div class="title wrap">อุ๊ปส์ ดูเหมือนว่าคุณจะมาถึงก่อนที่เราสร้าง Busble ให้เสร็จ</div>
+      <div class="tagline wrap">กรุณาลงชื่ออีเมล์ของคุณไว้ หาก Busble พร้อมใช้งานเมื่อไหร่ เราจะรีบแจ้งให้คุณทราบในทันที</div>
+    </div>
+    <form @submit.prevent="submit">
+      <input type="text" v-model="text_email">
+      <button type="submit" class="button primary">ลงทะเบียน</button>
+    </form>
   </div>
 </template>
 <script>
-
 export default {
-  name: 'register',
-  data () {
+  name: "register",
+  data() {
     return {
-      text_email: ''
-    }
+      text_email: ""
+    };
   },
   methods: {
     submit() {
-      const userListRef = Firebase.database().ref('/user')
-      let newUserRef = userListRef.push()
-      newUserRef.set({
-        email: this.text_email
-      }, function(error) {
-        if (error) {
-          // The write failed...
-        } else {
-            console.log('Data saved successfully!')
+      const userListRef = Firebase.database().ref("/user");
+      let newUserRef = userListRef.push();
+      newUserRef.set(
+        {
+          email: this.text_email
+        },
+        function(error) {
+          if (error) {
+            // The write failed...
+          } else {
+            console.log("Data saved successfully!");
+          }
         }
-      })
+      );
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  width: 100%;
+  max-width: 800px;
+  margin: auto;
+}
+.app-container {
+  text-align: center;
+  padding: 0 20px;
+  .process-mockup {
+    margin-top: 80px;
+    img {
+      width: 100%;
+      max-width: 350px;
+      height: auto;
+    }
+  }
+  .content {
+    margin-top: 30px;
+    .title {
+      position: relative;
+      font-size: 30px;
+      line-height: 40px;
+      font-weight: bold;
+      margin: 0 auto;
+      padding: 0;
+      color: #333333;
+    }
+    .tagline {
+      position: relative;
+      font-size: 20px;
+      line-height: 25px;
+      margin-top: 15px;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      padding-right: 20px;
+      color: #7b868a;
+    }
+  }
+  form {
+    margin-top: 20px;
+    margin-bottom: 80px;
+    input {
+      width: 100%;
+      max-width: 350px;
+      font-size: 18px;
+      font-weight: bold;
+      padding: 12px 24px;
+      margin: 10px;
+      border: 1px solid #E8EFF6;
+      border-radius: 3px;
+      color: #333;
+      background-color: #fff;
+      transition: all 0.2s ease;
+      //box-shadow: 0 5px 15px rgba(32, 43, 54, 0.05);
+    }
+    .button {
+      cursor: pointer;
+      display: inline-block;
+      font-size: 18px;
+      font-weight: bold;
+      padding: 12px 24px;
+      margin: 10px;
+      border: 1px solid #e9eff5;
+      border-radius: 3px;
+      color: #333;
+      background-color: #fff;
+      transition: all 0.2s ease;
+      box-shadow: 0 5px 15px rgba(32, 43, 54, 0.05);
+      &.primary {
+        border: 1px solid #0469b9;
+        color: #fff;
+        background-color: #0469b9;
+      }
+      &:hover {
+        box-shadow: 0 5px 15px rgba(32, 43, 54, 0.1);
+      }
     }
   }
 }
-</script>
-
-<style <style lang="scss" scoped>
-  .resgiter-form {
-    display: block;
-    margin: 0 auto;
-    &--dialog {
-      display: grid;
-      grid-template-rows: 25% 75%;
-      width: 500px;
-      height: 180px;
-      border-radius: 4px;
-      box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
-      .header {
-        h1 {
-          margin: 0;
-          padding: 1rem;
-          text-align: center;
-        }
-      }
-      .content {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 1rem;
-      }
-    }
-  }
 </style>
 
